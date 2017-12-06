ef343fc25d05ca63c46d8e630e2839f999f115f6
Priansh Shah <shah@priansh.com>
2017-12-06 16:13:01 +0000

    better rooms, collaborative editor, download as markdown

--- index.js
+++ index.js
@@ -16,9 +16,16 @@ io.on('connection', (skt) => {
         skt.join(r)
         scr = secret[Object.keys(io.sockets.adapter.rooms[r].sockets).length - 1]
         io.to(r).emit('joined', scr)
-    })    
+    })
     skt.on('chat message', (m) => {
         io.to(m.room).emit('chat message', m)
+    })
+    skt.on('refresh', (c) => {
+        io.to(c.room).emit('refresh', c)
+    })
+    skt.on('change', (c) => {
+        if (c.op.origin == '+input' || c.op.origin == 'paste' || c.op.origin == '+delete')
+            io.to(c.room).emit('change', c)
     })
 })
 
