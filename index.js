77977b8fa208e0b0972e71f35e453c8fcb36c427
Priansh Shah <shah@priansh.com>
2017-12-06 12:10:00 +0000

    put all code in html file, add rooms

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var clients = 0;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (q, s) => {
+    s.sendFile(__dirname + '/index.html')
+})
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+var secret = ['turquoise', 'coral', 'tan', 'gold'];
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+io.on('connection', (skt) => {
+    clients = clients + 1
+    skt.on('join', (r) => {
+        skt.join(r)
+        scr = secret[Object.keys(io.sockets.adapter.rooms[r].sockets).length - 1]
+        io.to(r).emit('joined', scr)
+    })    
+    skt.on('chat message', (m) => {
+        io.to(m.room).emit('chat message', m)
+    })
+})
+
+http.listen(port, () => {
+    console.log('Listening on *:' + port)
+})
