e12dc66f40ecefdc492a3f924e1844b5a3c91a5b
Priansh Shah <shah@priansh.com>
2017-12-11 00:39:20 +0000

    update doc

--- emoji-help/create_helpdoc.py
+++ emoji-help/create_helpdoc.py
@@ -5,8 +5,9 @@ print("%d emojis loaded"%len(data))
 colon_codes = []
 for emoji_data in data:
     for colon_code in emoji_data["short_names"]:
-        colon_codes.append(colon_code)
-_out = [" `:{c}:` | :{c}: ".format(c=code) for code in colon_codes]
+        colon_codes.append({"code":colon_code, "image":emoji_data["image"]})
+emoji_img_url = "https://github.com/iamcal/emoji-data/raw/340e3b897f71ca985ceccbd6b753e37776734f26/img-apple-64/"
+_out = [" `:{c}:` | ![{c}]({l}) ".format(c=code["code"], l=emoji_img_url+code["image"]) for code in colon_codes]
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield '|'+'|'.join(l[i:i + n])+'|'
