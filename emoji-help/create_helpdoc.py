86e12a1bba483a08c0d888d0327c55154d512f00
Priansh Shah <shah@priansh.com>
2017-12-11 00:31:17 +0000

    fix table outlay

--- emoji-help/create_helpdoc.py
+++ emoji-help/create_helpdoc.py
@@ -6,9 +6,9 @@ colon_codes = []
 for emoji_data in data:
     for colon_code in emoji_data["short_names"]:
         colon_codes.append(colon_code)
-_out = ["| `:{c}:` | :{c}: |".format(c=code) for code in colon_codes]
+_out = [" `:{c}:` | :{c}: ".format(c=code) for code in colon_codes]
 def chunks(l, n):
     for i in range(0, len(l), n):
-        yield ' '.join(l[i:i + n])
+        yield '|'+'|'.join(l[i:i + n])+'|'
 md_table = '\n'.join(list(chunks(_out, 5)))
 with open('README.md','w') as f: f.write(md_table)
