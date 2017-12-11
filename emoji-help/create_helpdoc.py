728662678e05095153e62e8cc939543e8faf48ec
Priansh Shah <shah@priansh.com>
2017-12-11 00:41:01 +0000

    fix format

--- emoji-help/create_helpdoc.py
+++ emoji-help/create_helpdoc.py
@@ -7,9 +7,9 @@ for emoji_data in data:
     for colon_code in emoji_data["short_names"]:
         colon_codes.append({"code":colon_code, "image":emoji_data["image"]})
 emoji_img_url = "https://github.com/iamcal/emoji-data/raw/340e3b897f71ca985ceccbd6b753e37776734f26/img-apple-64/"
-_out = [" `:{c}:` | ![{c}]({l}) ".format(c=code["code"], l=emoji_img_url+code["image"]) for code in colon_codes]
+_out = ["`:{c}:` | ![{c}]({l})".format(c=code["code"], l=emoji_img_url+code["image"]) for code in colon_codes]
 def chunks(l, n):
     for i in range(0, len(l), n):
-        yield '|'+'|'.join(l[i:i + n])+'|'
+        yield ' | '.join(l[i:i + n])
 md_table = '\n'.join(list(chunks(_out, 5)))
 with open('README.md','w') as f: f.write(md_table)
